24a277121f79de327651547e5af5b46ecbcfee74
shishkinamaria <shishkin11yandex.ru@gmail.com>
2024-12-04 12:41:21 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -8,8 +8,8 @@ async function getResponce() {
     let content = await responce.text()
     console.log("await responce.text()\n", content)
     content = JSON.parse(content)
-    content = content.slice(0, 9)
-    console.log("content.slice(0, 9)", content)
+    content = content.slice(0, 6)
+    console.log("content.slice(0, 6)", content)
     let key
     for (key in content) {
         console.log(content[key].id, content[key].title)
